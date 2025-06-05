af2554899d2665e518b10f3b504d13a98381af0c
Can Sever <cansevver01@gmail.com>
2025-06-05 14:33:02 +0200

    created columns, image and description of mine

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2, = st.columns(2)
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Can Sever")
+    content = """
+    I'm a Computer Engineering student based in Warsaw with hands-on experience in e-commerce and web development.
+    
+I ran an Etsy shop where I used Excel to analyze data and improve sales performance. I also interned as a Front-End Web Developer and gained practical skills in Python, Git, and JavaScript.
+
+I enjoy solving problems, learning new technologies, and building things. I'm currently looking for entry-level roles or internships in software or data-related fields.
+    """
+    st.info(content)
