dc743a8dd2c408eec3efa68d04848b198914fcfe
Can Sever <cansevver01@gmail.com>
2025-06-05 15:51:12 +0200

    added App  decriptions, images and links

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 #st.set_page_config(layout="wide")
 
-col1, col2, = st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/photo.png")
@@ -25,13 +25,18 @@ Below you can find some apps that I've built in Python. Feel free to contact me!
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 1, 1.5])
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
