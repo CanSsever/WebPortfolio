a411912ec5347c033c1f5a4702d2599e276d20c3
Can Sever <cansevver01@gmail.com>
2025-06-05 14:41:52 +0200

    Add description above projects

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 col1, col2, = st.columns(2)
 with col1:
     st.image("images/photo.png")
+
 
 with col2:
     st.title("Can Sever")
@@ -16,3 +17,8 @@ I ran an Etsy shop where I used Excel to analyze data and improve sales performa
 I enjoy solving problems, learning new technologies, and building things. I'm currently looking for entry-level roles or internships in software or data-related fields.
     """
     st.info(content)
+
+content2 = """
+Below you can find some apps that I've built in Python. Feel free to contact me!
+"""
+st.write(content2)
