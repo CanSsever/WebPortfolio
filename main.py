67c613e7f36209274ce7d518b20fb71cb35c62de
Can Sever <cansevver01@gmail.com>
2025-06-05 15:23:44 +0200

    render the titles in two different columns.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import streamlit as st
+import pandas
 
 #st.set_page_config(layout="wide")
 
 col1, col2, = st.columns(2)
+
 with col1:
     st.image("images/photo.png")
 
 
 with col2:
-    st.title("Can Sever")
+    st.title("CAN SEVER")
     content = """
     I'm a Computer Engineering student based in Warsaw with hands-on experience in e-commerce and web development.
     
@@ -22,3 +24,14 @@ content2 = """
 Below you can find some apps that I've built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
